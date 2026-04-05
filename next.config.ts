b0f775545d77db3f4f8942805754c80e3d876c8e
ashfad0273 <ashfadhossain0@gmail.com>
2026-04-05 15:39:56 +0600

    Final UI and Metadata polish

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: '/Ash-Flow',
-  assetPrefix: '/Ash-Flow/', // ADD THIS LINE
+  assetPrefix: '/Ash-Flow', // No trailing slash here
   images: {
     unoptimized: true,
   },
